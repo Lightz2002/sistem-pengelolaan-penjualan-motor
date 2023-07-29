bc718522c547e2cf98f753113aa87c1dd8bf7e8f
Ryan kenidy <kenidyryan@gmail.com>
2023-07-29 11:57:29 +0700

    select input default selected value

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,7 +1,8 @@
-<select name={{ $name }} id={{ $name }} {!! $attributes->merge([
+<select {!! $attributes->merge([
     'class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm',
 ]) !!}">
     @foreach ($options as $option)
-        <option value={{ $option->id }}>{{ ucwords($option->name) }}</option>
+        <option value={{ $option->id }} @if ($selected === $option->id) selected @endif>{{ ucwords($option->name) }}
+        </option>
     @endforeach
 </select>
