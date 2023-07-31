53958bcbc722b2a656f623d3c7811e9d892d8589
Ryan kenidy <kenidyryan@gmail.com>
2023-07-31 10:11:23 +0700

    add link type style for button

--- resources/views/components/redirect-button.blade.php
+++ resources/views/components/redirect-button.blade.php
@@ -1,10 +1,10 @@
 <a
     {{ $attributes->merge([
         'class' => 'px-2 text-center py-2 rounded-lg inline-flex items-center 
-                                    bg-indigo-500 border border-transparent rounded-md font-semibold text-xs
-                                    text-white uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700
-                                    active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500
-                                    focus:ring-offset-2 transition ease-in-out duration-150',
+                    bg-slate-500 border border-transparent rounded-md font-semibold text-xs
+                    text-white uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700
+                    active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500
+                    focus:ring-offset-2 transition ease-in-out duration-150',
     ]) }}>
     {{ $slot }}
 </a>
--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,23 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
+@php
+    // Set a default value for $type if it's not set
+$type = $type ?? 'submit';
+@endphp
+
+@if ($type === 'redirect')
+    <a
+        {{ $attributes->merge([
+            'class' => 'px-2 text-center py-2 rounded-lg inline-flex items-center 
+                                bg-gray-500 border border-transparent rounded-md font-semibold text-xs
+                                text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700
+                                active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500
+                                focus:ring-offset-2 transition ease-in-out duration-150',
+        ]) }}>
+        {{ $slot }}
+    </a>
+@else
+    <button
+        {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+        {{ $slot }}
+    </button>
+@endif
+
