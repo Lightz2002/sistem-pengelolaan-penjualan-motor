0e98ca3dfcd01cc5e084cc9895074b2770b65790
Ryan kenidy <kenidyryan@gmail.com>
2023-08-02 10:52:05 +0700

    select full width

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,5 +1,5 @@
 <select {!! $attributes->merge([
-    'class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm',
+    'class' => 'w-full mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm',
 ]) !!}">
     @foreach ($options as $option)
         <option value={{ $option->id }} @if ($selected === $option->id) selected @endif>{{ ucwords($option->name) }}
