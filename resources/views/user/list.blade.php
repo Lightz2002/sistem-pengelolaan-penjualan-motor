d84f05d27f7fe94a03b63d426b304d5d927e276e
Ryan kenidy <kenidyryan@gmail.com>
2023-08-03 17:15:21 +0700

    modify livewire pagination

--- resources/views/user/list.blade.php
+++ resources/views/user/list.blade.php
@@ -15,25 +15,12 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-8">
                 <h3 class="mb-4 text-lg font-bold">Users List</h3>
 
-                <!-- List Navbar -->
-                <div class="mb-4 flex items-baseline justify-evenly">
-                    <x-search-bar></x-search-bar>
-                    <x-primary-button type="redirect" class="ml-auto" href="/users/create">Create</x-primary-button>
-
-                </div>
-
                 <!-- Table -->
-                <div id="output">
-                    <x-table :headers="$headers" :datas="$users">
-
-                    </x-table>
-                </div>
-
-                {{-- Pagination --}}
-                {{ $users->links() }}
+                <livewire:users-table />
 
             </div>
         </div>
 
     </div>
 </x-app-layout>
+
