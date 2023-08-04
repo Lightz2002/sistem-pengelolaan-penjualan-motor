8609d4c0bb4e52e4e75b367487cf3259eed79fba
Ryan kenidy <kenidyryan@gmail.com>
2023-08-04 13:10:45 +0700

    admin data customer match *

--- resources/views/partials/admin-data-menu.blade.php
+++ resources/views/partials/admin-data-menu.blade.php
@@ -1,3 +1,2 @@
-<x-nav-link :href="route('customers')" :active="request()->is('customers')">
-    {{ __('Customers') }}
+<x-nav-link :href="route('customers')" :active="request()->is('customers*')"> {{ __('Customers') }}
 </x-nav-link>
