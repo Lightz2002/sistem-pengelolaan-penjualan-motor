e9a010b65905ac8b973aebb3cba115c008d9f4c7
Ryan kenidy <kenidyryan@gmail.com>
2023-08-04 13:09:04 +0700

    table sort function

--- app/Http/Livewire/Table.php
+++ app/Http/Livewire/Table.php
@@ -15,14 +15,38 @@ abstract class Table extends Component
     public $createUrl = '';
     protected $queryString = ['search'];
 
+    public $sortBy = 'created_at';
+
+    public $sortDirection = 'desc';
+
+
 
     public $page = 1;
     public abstract function query(): \Illuminate\Database\Eloquent\Builder;
     public abstract function columns(): array;
+
+    public function sort($key)
+    {
+        $this->resetPage();
+
+        if ($this->sortBy === $key) {
+            $direction = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+            $this->sortDirection = $direction;
+
+            return;
+        }
+
+        $this->sortBy = $key;
+        $this->sortDirection = 'asc';
+    }
+
     public function data()
     {
         return $this
             ->query()
+            ->when($this->sortBy !== '', function ($query) {
+                $query->orderBy($this->sortBy, $this->sortDirection);
+            })
             ->paginate($this->perPage);
     }
 
