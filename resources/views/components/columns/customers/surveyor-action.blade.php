9aa3bc3c7ebab34afa1e9a4103303713e8ce25b2
Ryan kenidy <kenidyryan@gmail.com>
2023-08-05 10:37:26 +0700

    change edit btn url and icon

--- resources/views/components/columns/customers/surveyor-action.blade.php
+++ resources/views/components/columns/customers/surveyor-action.blade.php
@@ -11,13 +11,12 @@
         </svg>
         <span>View</span>
     </a>
-    <a href={{ "/customers/{$id}" }}
+    <a href={{ "/customers/{$id}/edit" }}
         class="inline-flex items-center me-4 border bg-blue-400 text-white px-4 py-2 text-xs rounded-md">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="w-6 h-6 stroke-white me-1">
+            stroke="currentColor" class="w-4 h-4 stroke-white me-1">
             <path stroke-linecap="round" stroke-linejoin="round"
-                d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
         </svg>
         <span>Edit</span>
     </a>
