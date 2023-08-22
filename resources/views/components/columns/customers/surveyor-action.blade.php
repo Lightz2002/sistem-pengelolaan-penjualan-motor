1275143c9580053fb2837e783e04c6333d0223e7
ezb_ryankenidy <kenidyryan@gmail.com>
2023-08-22 15:45:36 +0700

    show acc/rej when pending

--- resources/views/components/columns/customers/surveyor-action.blade.php
+++ resources/views/components/columns/customers/surveyor-action.blade.php
@@ -20,21 +20,21 @@
         </svg>
         <span>Edit</span>
     </a>
-    @if ($row->sales_status === 'pending')
-    <span id="show-accept-alert"
-        class="inline-flex items-center me-4 border bg-green-400 text-white px-4 py-2 text-xs rounded-md">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-white me-1">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg>
-        <span>Accept</span>
-    </span>
-    <span id="show-reject-alert"
-        class="inline-flex items-center me-4 border bg-red-400 text-white px-4 py-2 text-xs rounded-md">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-white me-1">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-        <span>Reject</span>
-    </span>
+    @if ($row->sales_status === 'PENDING')
+        <span id="show-accept-alert"
+            class="inline-flex items-center me-4 border bg-green-400 text-white px-4 py-2 text-xs rounded-md">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-white me-1">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span>Accept</span>
+        </span>
+        <span id="show-reject-alert"
+            class="inline-flex items-center me-4 border bg-red-400 text-white px-4 py-2 text-xs rounded-md">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-white me-1">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span>Reject</span>
+        </span>
     @endif
 </div>
 
